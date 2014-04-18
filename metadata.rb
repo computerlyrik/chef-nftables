83f072986c6d609482638aab51b32669e160e5ab
Christian Fischer <sw-dev@computerlyrik.de>
2014-04-18 18:54:54 +0200

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'nftables'
+maintainer       'computerlyrik'
+maintainer_email 'chef-cookbooks@computerlyrik.de'
+license          'Apache 2.0'
+description      'Installs/Configures nftables'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports  'ubuntu','14.04'
